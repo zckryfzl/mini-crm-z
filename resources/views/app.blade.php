b604e784e048230b4f6f4d1b8a7abbe47c0c6188
zckryfzl <cptzackry2@gmail.com>
2026-08-09 16:20:47 +0800

    Complete Mini CRM Laravel assessment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<title inertia>
+    {{ config('app.name', 'FNXPERTS Mini CRM') }}
+</title>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" type="image/png" href="/public/favicon.ico">
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<link rel="icon" type="image/x-icon" href="/favicon.ico">
 
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
-</html>
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+<!-- Scripts -->
+@routes
+
+@viteReactRefresh
+
+@vite([
+    'resources/js/app.tsx',
+    "resources/js/Pages/{$page['component']}.tsx"
+])
+
+@inertiaHead
+</head>
+
+
+<body class="font-sans antialiased">
+
+    @inertia
+
+</body>
